96259214fc2d026383bf78a5cd00ca6d28862bc0
Elián <elian.fabian.el.belaizi@gmail.com>
2022-06-05 18:27:03 +0200

    second commit

---
+++ src/main/kotlin/com/elian/util/files/GBoardDictionaryParser.kt
@@ -0,0 +1,162 @@
+package com.elian.util.files
+
+import java.io.*
+
+private const val FILE_HEADER = "# Gboard Dictionary version:1"
+
+// This character is not a regular space character.
+private const val SEPARATOR = "	"
+
+object GBoardDictionaryParser
+{
+    fun getAllRecords(filepath: String): List<GBoardDictionaryRecord>
+    {
+        var line: String?
+
+        val records = mutableListOf<GBoardDictionaryRecord>()
+
+        BufferedReader(FileReader(filepath)).use { br ->
+
+            br.readLine() // skip the file header
+
+            while (br.readLine().also { line = it } != null)
+            {
+                val currentLine = line!!.split(SEPARATOR)
+
+                val currentRecord = GBoardDictionaryRecord(currentLine)
+
+                records.add(currentRecord)
+            }
+        }
+
+        return records
+    }
+
+    fun getRecord(filepath: String, key: String): GBoardDictionaryRecord?
+    {
+        var line: String?
+
+        BufferedReader(FileReader(filepath)).use { br ->
+
+            br.readLine() // skip the file header
+
+            while (br.readLine().also { line = it } != null)
+            {
+                val currentLine = line!!.split(SEPARATOR)
+
+                val currentRecord = GBoardDictionaryRecord(currentLine)
+
+                if (currentRecord.key == key)
+                {
+                    return currentRecord
+                }
+            }
+        }
+
+        return null
+    }
+
+    fun saveRecords(records: List<GBoardDictionaryRecord>, filepath: String)
+    {
+        // Records must be sorted by value before saving them, that's how GBoard dictionary works.
+        val sortedRecords = records.sortedBy { it.value }
+
+        BufferedWriter(FileWriter(filepath)).use { bw ->
+
+            bw.write(FILE_HEADER)
+            bw.newLine()
+
+            sortedRecords.forEach {
+                bw.write(it.toGBoardDictionary())
+                bw.newLine()
+            }
+        }
+    }
+
+    /**
+     * This method will be used when saving the records with its category
+     * but, you can't use it for a regular GBoard dictionary file.
+     */
+    fun saveRecordsWithCategory(records: List<GBoardDictionaryRecord>, filepath: String)
+    {
+        // Records must be sorted by value before saving them, that's how GBoard dictionary works.
+        val sortedRecords = records.sortedBy { it.value }
+
+        BufferedWriter(FileWriter(filepath)).use { bw ->
+
+            bw.write(FILE_HEADER)
+            bw.newLine()
+
+            sortedRecords.forEach {
+                bw.write(it.toGBoardDictionaryWithCategory())
+                bw.newLine()
+            }
+        }
+    }
+
+    /**
+     * Inserts the record in the appropriate place in the dictionary.
+     */
+    fun insertRecord(record: GBoardDictionaryRecord, filepath: String)
+    {
+        val records = getAllRecords(filepath) as MutableList<GBoardDictionaryRecord>
+
+        records.add(record)
+
+        val sortedRecords = records.sortedBy { it.value }
+
+        saveRecords(sortedRecords, filepath)
+    }
+
+    fun deleteRecord(record: GBoardDictionaryRecord, filepath: String)
+    {
+        val records = getAllRecords(filepath)
+
+        val newRecords = records.filter { it.key != record.key }
+
+        saveRecords(newRecords, filepath)
+    }
+}
+
+data class GBoardDictionaryRecord @JvmOverloads constructor(
+    var key: String,
+    var value: String,
+    /**
+     * ISO 639-1 language code.
+     */
+    var languageCode: String = "",
+
+    /**
+     * Extra property to group records.
+     */
+    var category: String = "",
+)
+{
+    constructor(fileLine: List<String>) : this(fileLine[0], fileLine[1], fileLine[2])
+
+    fun toGBoardDictionary(): String = listOf(key, value, languageCode).joinToString(SEPARATOR)
+
+    fun toGBoardDictionaryWithCategory(): String = listOf(key, value, languageCode, category).joinToString(SEPARATOR)
+
+    companion object
+    {
+        @JvmStatic
+        fun groupRecordsByCategory(records: List<GBoardDictionaryRecord>): Map<String, List<GBoardDictionaryRecord>>
+        {
+            val groupedRecords = mutableMapOf<String, MutableList<GBoardDictionaryRecord>>()
+
+            records.forEach {
+
+                val currentCategory = it.category
+
+                if (groupedRecords.containsKey(currentCategory))
+                {
+                    groupedRecords[currentCategory]?.add(it)
+                }
+                else groupedRecords[currentCategory] = mutableListOf(it)
+            }
+
+            return groupedRecords
+        }
+    }
+}
